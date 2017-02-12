888c61dfd2036d3596760e31f8a873d50d1a8e17
Akohas <doctorfreud33@gmail.com>
2017-02-12 23:32:07 +0300

    mogo service menu animate

--- mogo/js/dist/common.js
+++ mogo/js/dist/common.js
@@ -17,20 +17,22 @@ $(document).ready(function(){
 
 	$('.owl-loaded').addClass('owl-carousel');
 
-
-	var MenuItem = $('.service-menu__item');
-
+var MenuItem = $('.service-menu__item');
 	$('.service-menu__item').on('click', function(){
-				$(this).find('.service-menu__arrow').toggleClass('service-menu__arrow-top');
+		$('.service-menu__item').find('.service-menu__arrow').removeClass('service-menu__arrow-top');
 		if( $(this).children('.service-menu__hidden').hasClass('visible-menu') ){
 			$(this).children('.service-menu__hidden').removeClass('visible-menu');
+			$(this).children('.service-menu__hidden').slideUp(500);
+			$(this).find('.service-menu__arrow').removeClass('service-menu__arrow-top');
 		}else{
 			$('.service-menu__hidden').removeClass('visible-menu');
+			$('.service-menu__hidden').slideUp(500);
 			$(this).children('.service-menu__hidden').addClass('visible-menu');
+			$(this).children('.service-menu__hidden').slideDown(500);
+			$(this).find('.service-menu__arrow').addClass('service-menu__arrow-top');
 		}
 				
 	});
-
 
 });
 
--- mogo/js/modules/menu.js
+++ mogo/js/modules/menu.js
@@ -5,9 +5,12 @@ module.exports = function(){
 		$(this).find('.service-menu__arrow').toggleClass('service-menu__arrow-top');
 		if( $(this).children('.service-menu__hidden').hasClass('visible-menu') ){
 			$(this).children('.service-menu__hidden').removeClass('visible-menu');
+			$(this).children('.service-menu__hidden').slideUp(500);
 		}else{
 			$('.service-menu__hidden').removeClass('visible-menu');
+			$('.service-menu__hidden').slideUp(500);
 			$(this).children('.service-menu__hidden').addClass('visible-menu');
+			$(this).children('.service-menu__hidden').slideDown(500);
 		}
 				
 	});
